8b6cb32b14a65fcc444552d43187d510e9302c84
rajmahal4 <rsingh6@mail.sfsu.edu>
2020-08-19 17:01:28 -0700

    err

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -13,7 +13,8 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = process.env.DB_URL;
+var dev_db_url = process.env.DB_URL;
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
