c16d9a07cf4249df717a72949ff3a42473f35cf5
rajmahal4 <rsingh6@mail.sfsu.edu>
2022-07-04 10:09:41 -0700

    added photo sources and started category cards

--- models/category.js
+++ models/category.js
@@ -5,7 +5,8 @@ let Schema = mongoose.Schema;
 let CategorySchema = new Schema(
   {
     name: {type: String, required: true},
-    description: {type: String, required: true}
+    description: {type: String, required: true},
+    img_url: {type: String}
   }
 );
 
